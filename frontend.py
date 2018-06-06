70afe6edae5d26ea81d0876c85b8082820d9e90c
Simon Rowe <simonrowe@Simons-MacBook-Pro.local>
2018-06-06 22:57:03 +0100

    Simon

--- frontend.py
+++ frontend.py
@@ -1,3 +1,29 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
 import random
+from rollDice import funcRollDice
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return funcRollDice()
+        
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['numPlayers'] == '1':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+ 
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
 
-print(random.randint(1,101))
+
