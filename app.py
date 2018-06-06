8578b93cec545a287137ba6031d79bd161008e29
Simon Rowe <simonrowe@Simons-MacBook-Pro.local>
2018-06-06 22:54:50 +0100

    Simon

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+import random
+from rollDice import funcRollDice
  
 app = Flask(__name__)
  
+
+
 @app.route('/')
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Jack!"
+        return funcRollDice()
+        
  
 @app.route('/login', methods=['POST'])
 def do_admin_login():
@@ -22,3 +27,5 @@ def do_admin_login():
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=4000)
+
+
