1ce26197233e1e39010c85d0486c729a4c422701
Steven Gooday <StevenGooday@Stevens-MacBook-Pro.local>
2018-06-06 22:54:07 +0100

    Back end almost ther

--- BackEnd.py
+++ BackEnd.py
@@ -32,22 +32,44 @@ def createPlayerDict(numPlayers):
 # setup game and instantiate player dictionary
 createPlayerDict(setupPlayers())
 
-# declare ladder and snake array
-
 
 # play the game function
 def playGame() :
-  #while  playerDict[player] < 100 :
+  ladders = {5,15,25,35,45,55,65,75,85,95}
+  snakes = {10,20,30,40,50,60,70,80,90}
+
+# set winner default
   winner = 0
+
+  # loop through to player dictionary until you hit winner
   while winner == 0 :
+    
+    # loop through player dictionary
     for k in playerDict :
-        print("Player ", k,"'s turn, your current score is", playerDict[k])
-        input("Press enter to roll the dice")
+        input("Player ", k,"'s turn, your current score is", playerDict[k], ", press enter to roll the dice")
+        # roll the dice
         rollDice()
+        # update player score
         playerDict[k] = playerDict[k] + DiceValue
+        # feedback score to player
         print("You rolled a ", str(DiceValue), " and your new score is ", playerDict[k])
+
+        # test if the position landed is a ladder
+        for a in ladders :
+            if a == playerDict[k] : 
+                playerDict[k] = playerDict[k] + 3
+                print("Well done Cock, you landed on a ladder moving forward to ", playerDict[k] )
+
+        # test if the position landed on is a snake
+        for b in snakes :
+            if b == playerDict[k] : 
+                playerDict[k] = playerDict[k] - 3
+                print("How unfortunate you cunt, you landed on a snake moving back to ", playerDict[k] )
+
+        # test if the current players score is over 100
         if  playerDict[k] >= 100 :
             winner = [k]
+
     print("Player ", [k], "wins the game")
 
 # start game
